6b2779b4d3c3a8a164365f31a549c422620bd9bd
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-10 16:57:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 function happyHolidays() {
-  return Happy holidays!
+  return 'Happy holidays!'
 }
 
 function happyHolidaysTo(name) {
-  return Happy holidays, ${name}!
+  return 'Happy holidays, ${name}!''
 }
 
 function happyHolidayTo(holiday, name) {
