8747e5058ce7cc94da1319c9d2dccb72445e230e
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-10 17:12:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(days, holiday) {
-  return 'It's ' + days + ' days until ' + holiday + '!'
+  return 'It's' + ' ' days + ' days until ' + holiday + '!'
 }
