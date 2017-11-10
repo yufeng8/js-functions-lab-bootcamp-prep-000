2b67ae6e56ff0bda3cbb92b5669b8d7df3ca0354
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-10 16:43:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 // write your code below!
+var name = "Yufeng"
+var holiday = "Independent day"
+var days = 30
+
 function happyHolidays() {
-  return "Happy holidays!"
+  return 'Happy holidays!'
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
+  return "Happy holidays, ${name}!";
 }
 
 function happyHolidayTo(holiday, name) {
-  return 'Happy ${holiday}, ${name}!'
+  return 'Happy ${holiday}, ${name}!';
 }
 
 function holidayCountdown(days, holiday) {
-  return 'Its ${days} days until ${holiday}!'
+  return 'It's ${days} days until ${holiday}!';
 }
